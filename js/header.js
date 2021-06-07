d7e26db4f921883a4e09474684e22926b8aeb1e5
Feldsher <fedynuak.roma@gmail.com>
2021-06-07 17:19:29 +0300

    initial commit

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.header-burger').click(function(event) {
-        $('.header-burger, .menu').toggleClass('active');
+    $('.header-burger').click(function(event){
+        $('.heade-burger,.menu').toggleClass('active');
     });
 });
